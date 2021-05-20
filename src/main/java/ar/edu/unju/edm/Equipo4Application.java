e37be05084d4baba8f0e321ec7456f000ff925d8
daviid <deyvi@DESKTOP-34T29B1>
2021-05-20 17:48:22 -0300

    inicio

---
+++ src/main/java/ar/edu/unju/edm/Equipo4Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo4Application.class, args);
+	}
+
+}
